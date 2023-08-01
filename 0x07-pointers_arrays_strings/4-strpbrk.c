a3ba746a97d1e6b8a0f0ed0ba4a4b8251e987d91
manarxsalah <manarsalah15@gmail.com>
2023-08-01 03:58:29 +0300

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - A function that searches a string for any of a set of bytes.
+ * @s: starting address.
+ * @accept: char
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+		int i;
+
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+			if (*s == accept[i])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
