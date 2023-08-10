f36f6b1cc65eb09e4ae90fa151fef29a397eac62
manarxsalah <manarsalah15@gmail.com>
2023-08-10 17:46:24 +0300

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -47,11 +47,11 @@ int _atoi(const char *s)
 */
 void print_int(unsigned long int n)
 {
-	unsigned long int divisor = l, i, resp;
+	unsigned long int divisor = 1, i, resp;
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10)
 		;
-	for (; divisor >= 1; n %= divisor /= 10)
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
 		resp = n / divisor;
 		_putchar('0' + resp);
