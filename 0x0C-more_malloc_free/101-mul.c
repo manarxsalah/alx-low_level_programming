de634f21d7ee3ee5bbfbaba17020f669c3e87251
manarxsalah <manarsalah15@gmail.com>
2023-08-10 19:04:38 +0300

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,13 +6,13 @@
 */
 void _puts(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i])
-        {
-                _putchar(str[i]);
-                i++;
-        }
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
 /**
  * _atoi - convert a str to an int
@@ -21,24 +21,24 @@ void _puts(char *str)
 */
 int _atoi(const char *s)
 {
-        int sign = 1;
-        unsigned long int resp = 0, num1, i;
+	int sign = 1;
+	unsigned long int resp = 0, num1, i;
 
-        for (num1 = 0; !(s[num1] >= 48 && s[num1] <= 57); num1++)
-        {
-                if (s[num1] == '-')
-                {
-                        sign *= -1;
-                }
-        }
+	for (num1 = 0; !(s[num1] >= 48 && s[num1] <= 57); num1++)
+	{
+		if (s[num1] == '-')
+		{
+			sign *= -1;
+		}
+	}
 
-        for (i = num1; s[i] >= 48 && s[i] <= 57; i++)
-        {
-                resp *= 10;
-                resp += (s[i] - 48);
-        }
+	for (i = num1; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
 
-        return (sign * resp);
+	return (sign * resp);
 }
 /**
  * print_int - prints an int
@@ -47,15 +47,15 @@ int _atoi(const char *s)
 */
 void print_int(unsigned long int n)
 {
-        unsigned long int divisor = 1, i, resp;
+	unsigned long int divisor = 1, i, resp;
 
-        for (i = 0; n / divisor > 9; i++, divisor *= 10)
-                ;
-        for (; divisor >= 1; n %= divisor, divisor /= 10)
-        {
-                resp = n / divisor;
-                _putchar('0' + resp);
-        }
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
 }
 /**
  * main - print the result of the multiplication, followed by a new line
@@ -65,15 +65,14 @@ void print_int(unsigned long int n)
 */
 int main(int argc, char const *argv[])
 {
-        (void)argc;
+	(void)argc;
 
-        if (argc != 3)
-        {
-                _puts("Error\n");
-                exit(98);
-        }
-        print_int(_atoi(argv[1]) * _atoi(argv[2]));
-        _putchar('\n');
+	if (argc != 3)
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
 
-        return (0);
+	return (0);
 }
