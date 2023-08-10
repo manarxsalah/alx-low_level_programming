7800c2f3111efbf0762243c1ca5a0cce7362b2bc
manarxsalah <manarsalah15@gmail.com>
2023-08-10 15:15:03 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * string_nconcat - a function that concat two strings
  * @s1: ptr to the first str
@@ -29,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; s1[i]; != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 
 	for (j = 0; j < n; j++)
