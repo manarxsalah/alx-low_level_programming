c09067f9d72909647b2ea84ad5a9fabb76d87a28
manarxsalah <manarsalah15@gmail.com>
2023-08-14 19:34:40 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef DOG
-#define DOG
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog - a dog struct
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
- * Description: a struct "dog" that stores its name, age and the name of it's owner
+ * Description: a struct "dog" that stores its name, age
+ *		and the name of it's owner
 */
 struct dog
 {
