caf3e82047d51f3680b31591a6fc5a58088693d2
manarxsalah <manarsalah15@gmail.com>
2023-08-14 19:04:18 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: just a lone dog struct in a big kitty world
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
