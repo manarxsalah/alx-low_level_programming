b5855485aa7d812b09986857d6754cb9c0f19c08
manarxsalah <manarsalah15@gmail.com>
2023-08-16 13:59:57 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * @f: the printing function pointer
  * Return: void
 */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
