e66a45ec380ec6d6f30f8e89fde6dcbebda3fb4e
manarxsalah <manarsalah15@gmail.com>
2023-08-16 15:48:49 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for int
+ * @array: the int array
+ * @size: the array size
+ * @cmp: the function that compares
+ * Return: the int index
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
