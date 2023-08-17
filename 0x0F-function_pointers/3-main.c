a832a2a23b0fe1e24d8ccd76943e42dbf1c869d2
manarxsalah <manarsalah15@gmail.com>
2023-08-17 02:53:31 +0000

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,21 +7,24 @@
 */
 int main(int argc, char **argv)
 {
-	int (*op_func)(int, int), a, b;
+	int (*op_func)(int, int);
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("Error\n"), exit(99);
-
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(100);
-
-	printf("%d\n", op_func(a, b));
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if argv[2][1]
+	{
+		pringf("Error\n");
+		return (99);
+	}
+	fun = get_op_func(argv[2]);
+	if (fun == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	printf("%d\n", fun(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
