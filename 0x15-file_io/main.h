04f58b05dff4f7aede7d37adba55e3c6141b17f2
manarxsalah <manarsalah15@gmail.com>
2023-09-05 04:50:08 +0300

    elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <elf.h>
 
 #define READ_BUF_SIZE 1024
 #define USAGE "Usage: cp file_from file_to\n"
@@ -18,5 +19,6 @@ int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_osabi_more(Elf64_Ehdr h);
 
 #endif
